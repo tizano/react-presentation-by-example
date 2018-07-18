74c31e13aaac09728e8f016ecba9df831427bf66
Mathieu Scarlatella <ma.scarlatella@gmail.com>
2018-07-18 12:03:23 +0200

    core: Add .eslintrc.js and update package.json

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    // "indent": [2, 2, {"SwitchCase": 1}],
+    "no-magic-numbers": 0,
+    "react/prefer-es6-class": 0,
+    "react/no-multi-comp": 0,
+    "linebreak-style": 0,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/forbid-prop-types": 0,
+    "react/jsx-curly-brace-presence": [0, { "props": "ignore", "children": "ignore" }],
+    // "jsx-a11y/anchor-is-valid": [ "error", {
+    //   "components": [ "Link" ],
+    //   "specialLink": [ "to", "hrefLeft", "hrefRight" ],
+    //   "aspects": [ "noHref", "invalidHref", "preferButton" ]
+    // }]
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+  },
+  "plugins": [
+    "babel",
+    "import",
+  ]
+};
